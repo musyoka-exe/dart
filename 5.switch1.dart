8532cd603b607e6f381a31d149bcd83bb6869002
musyoka-exe <mkuru.podcast@gmail.com>
2026-01-03 15:17:02 +0300

    day 2 dart

---
+++ 5.switch1.dart
@@ -0,0 +1,19 @@
+void main(){
+    String grade = 'A';
+    switch(grade){
+        case 'A':
+            print('Excelent');
+            break;
+
+        case 'B':
+            print('Good');
+            break;
+
+        case 'C':
+            print('Pass');
+            break;
+
+        default:
+            print('Fail');
+    }
+}
